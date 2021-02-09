c2e50c0f98283c7fde910208c3279f2edac968a3
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-02-09 15:54:24 +0200

    default app settled

---
+++ src/component/Chat.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import io from "socket.io-client"
+
+
+class Chat extends React.Component {
+
+state={}
+
+
+
+
+
+
+render() {
+    return (
+
+    )
+}
+
+
+
+}
+export default  Chat
